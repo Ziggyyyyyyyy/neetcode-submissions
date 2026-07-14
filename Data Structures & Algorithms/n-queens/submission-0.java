7eab0c5dfc2ba2f8a7722f6b9eb76392c3a66d30
Aditi <168573382+Ziggyyyyyyyy@users.noreply.github.com>
2026-07-14 10:41:36 +0530

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans=new ArrayList<>();
+        char[][] board=new char[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
+        }
+        solve(0,board,ans,n);
+        return ans;
+    }
+    public void solve(int col,char[][] board,List<List<String>> ans,int n){
+        if(col==n){
+            List<String> temp=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                temp.add(new String(board[i]));
+            }
+            ans.add(temp);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row,col,board,n)){
+                board[row][col]='Q';
+                solve(col+1,board,ans,n);
+                board[row][col]='.';
+            }
+        }
+    }
+    public boolean isSafe(int row,int col,char[][] board,int n){
+        for(int j=col;j>=0;j--){
+            if(board[row][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=row,j=col;i<n && j>=0;i++,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+}
